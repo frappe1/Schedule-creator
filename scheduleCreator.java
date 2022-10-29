a66579c964e16f753e89fbde32c560c90527eb77
frappe1 <91944738+frappe1@users.noreply.github.com>
2022-10-29 19:51:27 +0200

    Hittar dat övningspass samt printar schemat snyggare

--- scheduleCreator.java
+++ scheduleCreator.java
@@ -9,8 +9,8 @@ public class Main {
 
         Activity fLecture = new Activity("Fysikföreläsning");
         Activity fExc = new Activity("Fysikövning");
-        Activity datLecture = new Activity("Datastruktföreläsning");
-        Activity datExc = new Activity("Datastruktövning");
+        Activity datLecture = new Activity("Datföreläsning");
+        Activity datExc = new Activity("Datövning");
         Activity gym = new Activity("Träning");
         Activity TApass = new Activity("Javapass");
         Activity TAprep = new Activity("TA-prep");
@@ -20,53 +20,70 @@ public class Main {
         sch[2][1] = fLecture;
         sch[3][1] = fLecture;
 
-        sch[0][3] = datLecture;
+        //sch[0][3] = datLecture;
         sch[1][3] = datLecture;
         sch[3][3] = datLecture;
 
         sch[1][2] = TApass;
 
         sch[1][0] = misc;
-        sch[3][3] = misc;
+        //sch[3][3] = misc;
         sch[4][0] = misc;
+        int[][] noGym = new int[2][2];
+        noGym[0][0] = 0;
+        noGym[0][1] = 0;
+        noGym[1][0] = 1;
+        noGym[1][1] = 1;
 
-        FindGymTime(gym);
+        FindGymTime(gym, noGym);
 
         FindPhysicsTime(fExc, fLecture);
 
-        FindTAPrep(TAprep, TApass, 2);
+        FindDatTime(datExc, datLecture);
 
-        //Print schedule
-        for(int d = 0; d < 5; d++){
-            System.out.print("\n\n%%%  " + weekday(d) + "  %%%%%");
-            for(int t = 0; t < 4; t++){
-                if(sch[d][t] != null){
-                    System.out.print("\n " + sch[d][t].name);
-                } else{
-                    System.out.print("\n []");
-                }
+        FindTAPrep(TAprep, TApass, 3);
 
-            }
+        String[] times = new String[]{"8-10", "10-12","13-15","15-17"};
+        System.out.printf("%n%-10s %-20s %-20s %-20s %-20s %-20s %n", "Tid:","Måndag:", "Tisdag:", "Onsdag:", "Torsdag:", "Fredag:");
+        for(int i = 0; i < 4; i++){
+            System.out.printf(" %-10s %-20s %-20s %-20s %-20s %-20s %n", times[i], nameMaybeNull(0, i), nameMaybeNull(1, i), nameMaybeNull(2, i), nameMaybeNull(3, i), nameMaybeNull(4, i));
         }
+
     }
 
-    public static void FindGymTime(Activity gym){
+    public static String nameMaybeNull(int d, int t){
+        if(sch[d][t] == null){
+            return "[]";
+        }
+        return sch[d][t].name;
+    }
+    public static void FindGymTime(Activity gym, int[][] noGym){
         int nextGymDay = 0;
         int[] times = new int[4];
         for(int i = 0; i < 4; i++){
             times[i] = -1;
         }
+        boolean break2 = false;
         for(int i = 0; i < 2; i++){
             //Searching the morning...
             Boolean foundGym = false;
             for(int d = nextGymDay; d < 5; d++){
                 for(int t = 0; t < 2; t++){
                     if(sch[d][t] == null){
-                        sch[d][t] = gym;
+                        break2 = false;
+                        for(int e = 0; e < noGym.length; e++){
+                            if(noGym[e][0] == d && noGym[e][1] == t){
+                                break2 = true;
+                            }
+                        }
+                        if(!break2){
+                            sch[d][t] = gym;
 
-                        nextGymDay = d + 2;
-                        foundGym = true;
-                        break;
+                            nextGymDay = d + 2;
+                            foundGym = true;
+                            break;
+                        }
+
                     }
                 }
                 if(foundGym){
@@ -140,6 +157,33 @@ public class Main {
                 } else if(sch[d][t] == TApass){
                     amount = 0;
                     break;
+                }
+            }
+        }
+    }
+
+    public static void FindDatTime(Activity datExc, Activity datLec){
+        int lecCounter = 0;
+        Boolean break2 = false;
+        for(int d = 0; d < 5 && (!break2); d++){
+            for(int t = 0; t < 4 && (!break2); t++){
+                if(sch[d][t] == datLec){
+                    lecCounter++;
+                    if(lecCounter == 3){
+                        for(int d2 = d; d2 < 5 && (!break2); d2++){
+                            for(int t2 = 0; t2 < 4; t2++){
+                                if(d2 == d){
+                                    t2 = t;
+                                }
+                                if(sch[d2][t2] == null){
+                                    sch[d2][t2] = datExc;
+                                    break2 = true;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+
                 }
             }
         }
